a520e92ddc62a51f044c63eb90979e9259ad598f
Zachary Harris <zachbharris94@gmail.com>
2019-03-28 12:35:12 -0500

    update to env and start to Macro model

---
+++ server/src/models/Macros.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const Macro = mongoose.Schema({});
+
+module.exports = mongoose.model("Macro", Macro);
--- server/src/passport/index.js
+++ server/src/passport/index.js
@@ -6,8 +6,8 @@ const User = require("../models/User");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CONSUMER_KEY,
-      clientSecret: process.env.GOOGLE_CONSUMER_SECRET,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/redirect"
     },
     (accessToken, refreshToken, profile, done) => {
